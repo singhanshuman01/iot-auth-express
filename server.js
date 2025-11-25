ff72e6e263308fcb29c68570314dd985ce3528f0
singhanshuman01 <singhanshuman592@gmail.com>
2025-11-25 12:04:24 +0530

    Added qrCode to direct user straight to start-charging route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import { server } from './src/services/websocket.js';
-import os from 'os';
+import { genQR } from './src/utils/qrGenerator.js';
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, '0.0.0.0', async ()=>{
     console.log(`Server listening, PORT: ${port}`);
-    console.log("Local IP address is: ", os.networkInterfaces().wlp2s0??[0].address);
+    if(os.networkInterfaces().wlp2s0){
+        console.log("Local IP address is: ", os.networkInterfaces().wlp2s0[0]?.address);
+        genQR();
+    }
 });
---
+++ src/utils/qrGenerator.js
@@ -0,0 +1,19 @@
+import qr from 'qr-image';
+import os from 'os';
+import fs from 'fs';
+
+var localIp = os.networkInterfaces().wlp2s0[0].address || null;
+
+function genQR(){
+    if(!localIp) return;
+
+    let url = `http://${localIp}:${process.env.PORT}/user/start-charging`;
+    console.log(url);
+
+    var qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream('qrToStartCharging.png'));
+
+    return;
+}
+
+export {genQR};
